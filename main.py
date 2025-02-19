f2848ce42d8239852215d37c242b66df34ff1b6f
qrivthv <pisofia@yandex.ru>
2025-02-19 11:20:06 +0300

    что-то произошло3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from functions import *
 from flask import *
 from math import *
+from matplotlib import *
 
 app = Flask(__name__)
 
@@ -16,23 +17,45 @@ def results():
         grade = 0
         region = 0
         time = 0
-        sorted(dt, key=lambda dt: dt[1])
         sorted(dt, key=lambda dt: dt[10])
+        sorted(dt, key=lambda dt: dt[-1])
         return render_template('res.html', dt=dt, time=time, tour=tour, grade=grade, region=region)
     elif request.method == 'POST':
         tour = request.form['tour']
         grade = request.form['grade']
         region = request.form['region']
         time = request.form['time']
+        if time == "":
+            time = 0
+        if grade == "":
+            grade = 0
+        if region == "":
+            region = 0
+        if tour == "":
+            tour = 0
         if time%5 != 0:
-            round(time)
+            round(time/5)*5
+        if time > 300:
+            time =300
         dt = get_data("result", tour, time, grade, region)
-        sorted(dt, key=lambda dt: dt[1])
-        sorted(dt, key=lambda dt: dt[10])
+        sorted(dt, key=lambda dt: dt[0])
+        sorted(dt, key=lambda dt: dt[-1])
         return render_template('res.html', dt=dt, time=time, tour=tour, grade=grade, region=region)
 
 
+@app.route('/schools')
+def by_school(id = -1):
+    #dt = get_data("schools", id)
+    dt = [['school1', 'Санкт-Петербург', 100, 45, 5], ['school2', 'Санкт-Петербург', 90, 35, 2]]
+    #table=get_school_table()
+    table = ""
+    return render_template('results.html', table=table)
 
+
+@app.route('/student/<name>')
+def student(name):
+    table = get_student_info(name)
+    return render_template('student.html')
 
 
 if __name__ == "__main__":
