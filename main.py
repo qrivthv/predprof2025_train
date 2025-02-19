a5283b63da7bcd879610ab879017d2f86346e311
qrivthv <pisofia@yandex.ru>
2025-02-19 10:19:46 +0300

    что-то произошло

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 from functions import *
 from flask import *
+from sql_func import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/results', m)
+def results():
+    return render_template()
+
+
+
+if __name__ == "__main__":
+    app.run(port=8080, host="127.0.0.1")
